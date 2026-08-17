470281e1e0ef38fcbdac38bc140c19f515f1781b
Phan Nguyen Quoc Minh <minhpnq1807@gmail.com>
2026-08-18 00:41:45 +0700

    fix: allow production origin in Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["fogewise.io.vn"],
+    },
+  },
 };
 
 export default nextConfig;
